418c8a4d5e03b2bddd82c4b0f6ecccf98936d587
kingkongs7 <1129594022@qq.com>
2021-04-21 15:43:12 +0800

    "part 4 test"

--- main.c
+++ main.c
@@ -15,7 +15,11 @@ typedef enum {
 } MetaCommandResult;
 
 typedef enum {
-    PREPARE_SUCCESS,PREPARE_SYNTAX_ERROR, PREPARE_UNRECOGNIZED_STATEMENT
+    PREPARE_SUCCESS,
+    PREPARE_SYNTAX_ERROR,
+    PREPARE_UNRECOGNIZED_STATEMENT,
+    PREPARE_STRING_TOO_LONG
+    PREPARE_NEGATIVE_ID,
 } PrepareResult;
 
 typedef enum {
@@ -31,8 +35,8 @@ typedef enum {
 #define COLUMN_EMAIL_SIZE 255
 typedef struct {
     uint32_t id;
-    char username[COLUMN_USERNAME_SIZE];
-    char email[COLUMN_EMAIL_SIZE];
+    char username[COLUMN_USERNAME_SIZE + 1];
+    char email[COLUMN_EMAIL_SIZE + 1];
 } Row;
 typedef struct {
     StatementType type;
@@ -59,7 +63,7 @@ typedef struct {
     void *pages[TABLE_MAX_PAGES];
 } Table;
 
-void print_row(Row* row) {
+void print_row(Row *row) {
     printf("(%d, %s, %s)\n", row->id, row->username, row->email);
 }
 
@@ -100,7 +104,7 @@ Table *new_table() {
 }
 
 // 释放表
-void free_table(Table* table) {
+void free_table(Table *table) {
     for (int i = 0; table->pages[i]; i++) {
         free(table->pages[i]);
     }
@@ -156,18 +160,40 @@ MetaCommandResult do_meta_command(InputBuffer *input_buffer) {
         return META_COMMAND_UNRECOGNIZED_COMMAND;
 }
 
-// 相当于数据库的SQL compiler 识别命令
-// 检测非点开头的命令是否是正确的命令
+// 分析器(parser) insert处理，使用strtok分割，检查相关内容是否符合标准
+PrepareResult prepare_insert(InputBuffer *input_buffer, Statement *statement) {
+    statement->type = STATEMENT_INSERT;
+    char *keyword = strtok(input_buffer->buffer, " ");
+    char *id_string = strtok(NULL, " ");
+    char *username = strtok(NULL, " ");
+    char *email = strtok(NULL, " ");
+
+    if (id_string == NULL || username == NULL || email == NULL) {
+        return PREPARE_SYNTAX_ERROR;
+    }
+
+    int id = atoi(id_string);
+    if (id < 0) {
+        return PREPARE_NEGATIVE_ID;
+    }
+    if (strlen(username) > COLUMN_USERNAME_SIZE) {
+        return PREPARE_STRING_TOO_LONG;
+    }
+    if (strlen(email) > COLUMN_EMAIL_SIZE) {
+        return PREPARE_STRING_TOO_LONG;
+    }
+
+    statement->row_to_insert.id = id;
+    strcpy(statement->row_to_insert.username, username);
+    strcpy(statement->row_to_insert.email, email);
+
+    return PREPARE_SUCCESS;
+}
+
+// 相当于数据库的SQL compiler 识别命令 parser
 PrepareResult prepare_statement(InputBuffer *input_buffer, Statement *statement) {
     if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
-        statement->type = STATEMENT_INSERT;
-        int args_assigned = sscanf(
-                input_buffer->buffer, "insert %d %s %s", &(statement->row_to_insert.id),
-                statement->row_to_insert.username, statement->row_to_insert.email);
-        if (args_assigned < 3) {
-            return PREPARE_SYNTAX_ERROR;
-        }
-        return PREPARE_SUCCESS;
+        return prepare_insert(input_buffer,statement);
     }
     if (strncmp(input_buffer->buffer, "select", 6) == 0) {
         statement->type = STATEMENT_SELECT;
@@ -251,6 +277,12 @@ int main() {
             case (PREPARE_SYNTAX_ERROR):
                 printf("Syntax error. Could not parse statement.\n");
                 continue;
+            case (PREPARE_STRING_TOO_LONG):
+                printf("String is too long.\n");
+                continue;
+            case (PREPARE_NEGATIVE_ID):
+                printf("ID must be positive.\n");
+                continue;
             case (PREPARE_UNRECOGNIZED_STATEMENT):
                 printf("Unrecognized keyword at start of '%s'.\n", input_buffer->buffer);
                 continue;
@@ -258,11 +290,11 @@ int main() {
 
         switch (execute_statement(&statement, table)) {
             case (EXECUTE_SUCCESS):
-                printf("0Executed.\n");
+                printf("Executed.\n");
                 break;
-                case (EXECUTE_TABLE_FULL):
-                    printf("Error: Table full.\n");
-                    break;
+            case (EXECUTE_TABLE_FULL):
+                printf("Error: Table full.\n");
+                break;
         }
     }
 }
